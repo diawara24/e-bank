3a9c865c3331e8804ee5fe2810ce2a0c1a3219b5
diawara24 <mourtallafatydiawara@gmail.com>
2023-05-24 13:29:16 +0200

    Mongoose Database connection

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected !'))
+.catch(() => console.log('Erreur with MongoDB connection'));
 
 
 module.exports = app;
