f2cd8b57ad0f5b542aa96d81575a726af7fe56d8
diawara24 <mourtallafatydiawara@gmail.com>
2023-05-24 16:33:27 +0200

    database change

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
+const connectDB = require('./app/models/index.js');
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB connected !'))
-.catch(() => console.log('Erreur with MongoDB connection'));
+// connexion a la base donnee
+connectDB();
 
 
 module.exports = app;
